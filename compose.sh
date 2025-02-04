98b454afe2c979c2882a27b6031ec4b47a1c39a6
chpalex <alex.olya@gmail.com>
2025-02-04 13:13:55 -0500

    compose update

--- compose.sh
+++ compose.sh
@@ -44,7 +44,7 @@ services:
       --dns-duckdns-propagation-seconds 60 -d "$jks_domain"
   nginx:
     container_name: ${container_name}_nginx
-    build: ./nginx
+    build: /nginx
     ports:
       - 80:80
       - 444:443
@@ -55,7 +55,7 @@ services:
     restart: unless-stopped
   wowza:
     container_name: ${container_name}
-    build: ./wowza
+    build: /wowza
     restart: always
     ports:
       - "6970-7000:6970-7000/udp"
