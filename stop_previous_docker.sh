47c379b215af618cc72bcfc497ef32a13e69931f
chpalex <alex.olya@gmail.com>
2025-02-05 20:14:52 -0500

    a

--- stop_previous_docker.sh
+++ stop_previous_docker.sh
@@ -3,7 +3,7 @@
 # Function to stop docker containers that have the same ports as the compose stack
 stop_previous_docker() {
   # Extract ports from the docker-compose.yml file
-  ports=$(grep -oP 'ports:\n\s*-\s*\K[0-9]+' "$container_dir/docker-compose.yml")
+  ports=$(sudo grep -oP 'ports:\n\s*-\s*\K[0-9]+' "$container_dir/docker-compose.yml")
 
   # Check for running containers with matching ports
   for port in $ports; do
