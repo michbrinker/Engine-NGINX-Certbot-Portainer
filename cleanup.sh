e4c8ed0284bd6b9349bb68246a7489e59aeb2aea
chpalex <alex.olya@gmail.com>
2025-02-05 19:43:38 -0500

    add .sh removal from encp remove tomcat file from upload try to create a path volume for engine

--- cleanup.sh
+++ cleanup.sh
@@ -3,11 +3,13 @@
 ####
 # Function to clean up the install directory and prompt user to delete Docker images and containers
 cleanup() {
-echo "Cleaning up the install directory..."
+  echo "Cleaning up the install directory..."
 
-  #if [ -f "$upload/tomcat.properties" ]; then
-  #  sudo rm "$upload/tomcat.properties"
-  #fi
+  sudo rm $encp/*.sh
+
+  if [ -f "$upload/tomcat.properties" ]; then
+    sudo rm "$upload/tomcat.properties"
+  fi
 
   # Restart docker stack to apply changes
   cd $container_dir
---
+++ mount_engine_conf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mount_engine_conf () {
+# Copy Wowza install directory
+sudo docker cp "$container_name:/usr/local/WowzaStreamingEngine/" $container_dir/
+
+# Replace the volume mount
+sed -i 's|- engine:/usr/local/WowzaStreamingEngine|./WowzaStreamingEngine:/usr/local/WowzaStreamingEngine|' docker-compose.yml
+
+# Rebuild docker compose stack
+sudo docker compose down && sudo docker compose up -d
+}
