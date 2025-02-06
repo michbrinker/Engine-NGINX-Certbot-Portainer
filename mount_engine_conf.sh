87a002b5772eb055afbc079df62ec6765abd1412
chpalex <alex.olya@gmail.com>
2025-02-05 20:10:02 -0500

    fix typo

--- mount_engine_conf.sh
+++ mount_engine_conf.sh
@@ -7,7 +7,7 @@ sudo docker cp "$container_name:/usr/local/WowzaStreamingEngine/" $container_dir
 # Replace the volume mount
 sed -i 's|- engine:/usr/local/WowzaStreamingEngine|- ./WowzaStreamingEngine:/usr/local/WowzaStreamingEngine|' docker-compose.yml
 # Remove the engine volume definition
-sed -i '/engine:/,/driver: local/d'
+sed -i '/engine:/,/driver: local/d' docker-compose.yml
 
 # Rebuild docker compose stack
 sudo docker compose down && sudo docker compose up -d
