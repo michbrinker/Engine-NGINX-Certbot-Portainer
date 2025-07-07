3274c30e43bd54f53542b235176f8e6561288b81
alex-chepurnoy <59021023+alex-chepurnoy@users.noreply.github.com>
2025-07-07 10:29:22 -0400

    enable mount_engine_conf and add 5 second sleep

--- installer.sh
+++ installer.sh
@@ -113,8 +113,9 @@ fi
 swagger
 cleanup
 create_html_instructions
-# Disabling the mount_engine_conf function for now to test
-# mount_engine_conf
+# adding a sleep here to resolve 4.9.5 issue with mount_engine_conf copying and removing the image too early
+sleep 5
+mount_engine_conf
 
 if $use_ssl; then
 echo -e "${w}For instructions on using the installed software, please visit ${yellow}https://$jks_domain:444/instructions.html${NOCOLOR}"
